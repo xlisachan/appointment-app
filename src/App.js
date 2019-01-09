570f4dcd2d2117f21694122a2ac4b9001a7251d8
xlisachan <xlisachan@gmail.com>
2019-01-09 17:10:31 -0500

    Created main interface component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class MainInterface extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="interface">
+        <h1>Pet Appointments</h1>
+        <ul>
+          <li>Buffy 3:30PM</li>
+          <li>Spot 5:30PM</li>
+          <li>Goldie 8:30AM</li>
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default MainInterface;
