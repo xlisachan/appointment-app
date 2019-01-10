98342be936729f6f0c808e9e4f3bff8e214bd999
xlisachan <xlisachan@gmail.com>
2019-01-09 19:13:50 -0500

    Updated a tags to button tags

--- src/SearchAppointments.js
+++ src/SearchAppointments.js
@@ -22,12 +22,12 @@ class SearchAppointments extends Component {
                             <button type="button" className="btn btn-primary dropdown-toggle"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sort by: <span className="caret"></span></button>
                             <ul className="dropdown-menu dropdown-menu-right">
-                                <li><a href="#" id="petName" onClick={ this.handleSort }>Pet Name {(this.props.orderBy === 'petName') ? <span className="glyphicon glyphicon-ok"></span> : null }</a></li>
-                                <li><a href="#" id="aptDate" onClick={ this.handleSort }>Date {(this.props.orderBy === 'aptDate') ? <span className="glyphicon glyphicon-ok"></span> : null }</a></li>
-                                <li><a href="#" id="ownerName" onClick={ this.handleSort }>Owner {(this.props.orderBy === 'ownerName') ? <span className="glyphicon glyphicon-ok"></span> : null }</a></li>
+                                <li><button href="#" id="petName" onClick={ this.handleSort }>Pet Name {(this.props.orderBy === 'petName') ? <span className="glyphicon glyphicon-ok"></span> : null }</button></li>
+                                <li><button href="#" id="aptDate" onClick={ this.handleSort }>Date {(this.props.orderBy === 'aptDate') ? <span className="glyphicon glyphicon-ok"></span> : null }</button></li>
+                                <li><button href="#" id="ownerName" onClick={ this.handleSort }>Owner {(this.props.orderBy === 'ownerName') ? <span className="glyphicon glyphicon-ok"></span> : null }</button></li>
                                 <li role="separator" className="divider"></li>
-                                <li><a href="#" id="asc" onClick={ this.handleOrder }>Asc {(this.props.orderDir === 'asc') ? <span className="glyphicon glyphicon-ok"></span> : null }</a></li>
-                                <li><a href="#" id="desc" onClick={ this.handleOrder }>Desc {(this.props.orderDir === 'desc') ? <span className="glyphicon glyphicon-ok"></span> : null }</a></li>
+                                <li><button href="#" id="asc" onClick={ this.handleOrder }>Asc {(this.props.orderDir === 'asc') ? <span className="glyphicon glyphicon-ok"></span> : null }</button></li>
+                                <li><button href="#" id="desc" onClick={ this.handleOrder }>Desc {(this.props.orderDir === 'desc') ? <span className="glyphicon glyphicon-ok"></span> : null }</button></li>
                             </ul>
                         </div>
                     </div>
