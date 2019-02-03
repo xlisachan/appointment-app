163d9060b3f9a7a5f6572548a94db7a80414951d
xlisachan <xlisachan@gmail.com>
2019-02-03 11:06:54 -0500

    Updates

---
+++ precache-manifest.3bb781a2ed529a24c47a237d5c73bb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c67f3afb67fe3af0fe",
+    "url": "/react_appointment-app/static/css/main.9e40f8d6.chunk.css"
+  },
+  {
+    "revision": "48c67f3afb67fe3af0fe",
+    "url": "/react_appointment-app/static/js/main.48c67f3a.chunk.js"
+  },
+  {
+    "revision": "5238454f6dec571f037e",
+    "url": "/react_appointment-app/static/js/1.5238454f.chunk.js"
+  },
+  {
+    "revision": "964c1bf1918e1ffc15cd",
+    "url": "/react_appointment-app/static/js/runtime~main.964c1bf1.js"
+  },
+  {
+    "revision": "e87dec9a071f27e919d5b234e80ec1af",
+    "url": "/react_appointment-app/static/media/wisdompetlogo.e87dec9a.svg"
+  },
+  {
+    "revision": "61c561799048c64c32b1fa132b9476ef",
+    "url": "/react_appointment-app/index.html"
+  }
+];
